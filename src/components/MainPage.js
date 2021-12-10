36222c509022d1645ff8ee3b9e729f330c62723e
Callum <callum@local>
2021-12-10 20:50:58 +0200

    sa covid

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -11,6 +11,17 @@ function MainPage() {
   return (
     <main className="flex flex-col h-screen md:max-w-screen-lg mx-auto">
       <div className="flex-grow">
+        <div className="mx-4 my-4 p-2 rounded-md bg-red-200 border-2 border-red-500 text-red-600">
+          <p>
+            For all official information and updates regarding the COVID-19
+            pandemic please visit the South African Department of Health's
+            website at:
+            <a href="https://www.sacoronavirus.co.za" target="_sa_covid">
+              {" "}
+              sacoronavirus.co.za
+            </a>
+          </p>
+        </div>
         <IntroductionCard />
         <ServicesCard />
         <ContactCard />
